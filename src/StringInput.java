832a34548809dc90c910371c2973b83f5b9be1a3
marclmao <119444616+marclmao@users.noreply.github.com>
2023-09-27 13:47:35 -0400

    fix this dumbass

--- src/StringInput.java
+++ src/StringInput.java
@@ -1,2 +1,14 @@
-public class StringInput {
+import java.util.Scanner;
+public class StringInput
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("whats yo name ");
+        String name = scan.nextLine();
+
+        System.out.println("wassup +" name);
+
+
+    }
 }
