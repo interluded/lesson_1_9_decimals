a7028a05e2f5334fdafac14a780cc9b771b79a9e
marclmao <119444616+marclmao@users.noreply.github.com>
2023-09-27 13:41:40 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first integer ");
+        double integer_1 = scan.nextDouble();
+        System.out.print("Enter the second integer ");
+        double integer_2 = scan.   nextDouble();
+        System.out.print("Enter the third integer ");
+        double integer_3 = scan.nextDouble();
+        System.out.println((integer_1 + integer_2 + integer_3) /  3.0);
+
+
+
+    }
+}
